b360a6d63041a852af68cab9aee81c4240fccf8f
enolgor <enolgor@teleco.upv.es>
2020-12-09 13:52:18 +0100

    Refactor logger

--- battlenet/logger.go
+++ battlenet/logger.go
@@ -1,60 +1,101 @@
 package battlenet
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 )
 
-type LogLevel int
+type LogLevel string
 
 const (
-	DEBUG LogLevel = iota
-	INFO
-	WARN
-	ERROR
+	DEBUG = "DEBUG"
+	INFO  = "INFO"
+	WARN  = "WARN"
+	ERROR = "ERROR"
 )
 
 type battlenetLogger interface {
-	SetLogOutput(writer io.Writer, levels ...LogLevel)
-	getLogger(level LogLevel) *log.Logger
+	SetLogger(logger *log.Logger, levels ...LogLevel)
+	getLogger(level LogLevel) bncLogger
 }
 
 type battlenetLoggerImpl struct {
-	debugLogger *log.Logger
-	infoLogger  *log.Logger
-	errorLogger *log.Logger
-	warnLogger  *log.Logger
+	loggers map[LogLevel]*bncLoggerImpl
 }
 
 func newBattleNetLogger() battlenetLogger {
-	bli := &battlenetLoggerImpl{}
-	bli.debugLogger = log.New(ioutil.Discard, "DEBUG: ", log.LstdFlags)
-	bli.infoLogger = log.New(ioutil.Discard, "INFO: ", log.LstdFlags)
-	bli.errorLogger = log.New(ioutil.Discard, "ERROR: ", log.LstdFlags)
-	bli.warnLogger = log.New(ioutil.Discard, "WARN: ", log.LstdFlags)
+	bli := &battlenetLoggerImpl{loggers: make(map[LogLevel]*bncLoggerImpl)}
+	bli.loggers[DEBUG] = &bncLoggerImpl{level: DEBUG, internal: nil}
+	bli.loggers[INFO] = &bncLoggerImpl{level: INFO, internal: nil}
+	bli.loggers[ERROR] = &bncLoggerImpl{level: ERROR, internal: nil}
+	bli.loggers[WARN] = &bncLoggerImpl{level: WARN, internal: nil}
 	return bli
 }
 
-func (bli *battlenetLoggerImpl) getLogger(level LogLevel) *log.Logger {
-	switch level {
-	case INFO:
-		return bli.infoLogger
-	case DEBUG:
-		return bli.debugLogger
-	case ERROR:
-		return bli.errorLogger
-	case WARN:
-		return bli.warnLogger
+func (bli *battlenetLoggerImpl) getLogger(level LogLevel) bncLogger {
+	log, ok := bli.loggers[level]
+	if !ok {
+		log = bli.loggers[INFO]
 	}
-	panic("Missing logger in switch")
+	return log
 }
 
-func (bli *battlenetLoggerImpl) SetLogOutput(writer io.Writer, levels ...LogLevel) {
+func (bli *battlenetLoggerImpl) SetLogger(logger *log.Logger, levels ...LogLevel) {
 	if len(levels) == 0 {
 		levels = []LogLevel{INFO, ERROR, WARN, DEBUG}
 	}
 	for _, level := range levels {
-		bli.getLogger(level).SetOutput(writer)
+		bli.loggers[level].internal = logger
 	}
+}
+
+type bncLogger interface {
+	Print(v ...interface{})
+	Printf(format string, v ...interface{})
+	Println(v ...interface{})
+	Writer() io.Writer
+}
+
+type bncLoggerImpl struct {
+	level    LogLevel
+	internal *log.Logger
+}
+
+func (bli *bncLoggerImpl) Print(v ...interface{}) {
+	if bli.internal == nil {
+		return
+	}
+	prefix := bli.internal.Prefix()
+	bli.internal.SetPrefix(fmt.Sprintf("[%s]", bli.level))
+	bli.internal.Print(v...)
+	bli.internal.SetPrefix(prefix)
+}
+
+func (bli *bncLoggerImpl) Printf(format string, v ...interface{}) {
+	if bli.internal == nil {
+		return
+	}
+	prefix := bli.internal.Prefix()
+	bli.internal.SetPrefix(fmt.Sprintf("[%s]", bli.level))
+	bli.internal.Printf(format, v...)
+	bli.internal.SetPrefix(prefix)
+}
+
+func (bli *bncLoggerImpl) Println(v ...interface{}) {
+	if bli.internal == nil {
+		return
+	}
+	prefix := bli.internal.Prefix()
+	bli.internal.SetPrefix(fmt.Sprintf("[%s]", bli.level))
+	bli.internal.Println(v...)
+	bli.internal.SetPrefix(prefix)
+}
+
+func (bli *bncLoggerImpl) Writer() io.Writer {
+	if bli.internal == nil {
+		return ioutil.Discard
+	}
+	return bli.internal.Writer()
 }
